180bdc5b0b2903c7d3dd6537517d7244e27d36d4
Samuel Desbos <samuel.desbos@gmail.com>
2020-07-10 15:49:57 +0200

    nextjs version

--- server/src/sendRefreshToken.ts
+++ server/src/sendRefreshToken.ts
@@ -5,7 +5,8 @@ export const sendRefreshToken = (res: Response, token: string) => {
         token,
         {
             httpOnly: true,
-            path: '/refresh-token'
+            // see https://www.youtube.com/watch?v=ZAsZ2hhdj-s 4:00
+            // path: '/refresh-token'
         }
     );
 };
